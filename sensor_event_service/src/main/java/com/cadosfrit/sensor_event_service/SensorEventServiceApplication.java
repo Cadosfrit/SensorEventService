b202e7b8f8b93d50cab883a323c447fa7c7fba23
Ronit Roshan Nayak <ronitroshan2003@gmail.com>
2026-01-06 21:33:50 +0530

    [v0.1] Initial Commit

---
+++ sensor_event_service/src/main/java/com/cadosfrit/sensor_event_service/SensorEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cadosfrit.sensor_event_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorEventServiceApplication.class, args);
+	}
+
+}
---
+++ sensor_event_service/src/test/java/com/cadosfrit/sensor_event_service/SensorEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadosfrit.sensor_event_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorEventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
